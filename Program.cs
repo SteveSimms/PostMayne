32b0dbd599dfca4497ab5e56433bd5b60ac75f53
Steven Simms <simmsstev@gmail.com>
2021-11-15 15:52:29 -0800

    Have client and server communicating

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Asim");
-app.MapGet("/test"() => "Test 1,2")
+app.MapGet("/test", () => "Test 1,2");
 
 app.Run();
--- post-mayne-client/src/components/HelloWorld.vue
+++ post-mayne-client/src/components/HelloWorld.vue
@@ -1,40 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+    import { ref } from 'vue'
+    import axios from 'axios'
+    let test = ref('Yo!!!')
 
-defineProps({
-  msg: String
-})
+    defineProps({
+        msg: String
+    })
 
-const count = ref(0)
+    const count = ref(0)
+
+    let getTest = async () => {
+        await axios.get('http://localhost:5164/test')
+            .then((response) => {
+                test.value = response.data
+                console.log(test.value)
+                return test.value
+            })
+    }
+    getTest()
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <h1>{{ msg }}</h1>
+    <h1>{{    test }} </h1>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 </style>
