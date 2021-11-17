f194f9111fe4d88d83e2027828c36017771b779b
Steven Simms <simmsstev@gmail.com>
2021-11-16 16:57:01 -0800

    .NET 6 has a new and simple way to start the spa server and backend server at the same time on the same domain

--- Program.cs
+++ Program.cs
@@ -9,6 +9,12 @@ app.MapGet("/get", () => "Status 200 OK");
 app.MapPost("/post", () => "I'm Posted");
 //Todo create a db to store the test posts and puts will have to make a GET to post and post to a GET
 
+app.UseRouting();
+//Use static files and MapFallBackToFile are critical to running spa(Vue/vite) server at the same time
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
+//app.UseHttpsRedirection("http://localhost:3000");
+
 
 
 
